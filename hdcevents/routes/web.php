f16fabe0f22b46942cf2d2bed5cace1d6007505f
Matheus <msgoncalves@inf.ufpel.edu.br>
2023-11-28 21:03:19 -0300

    Getting started with Laravel

---
+++ hdcevents/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $nome = "Matheus";
+    $idade = 21;
+
+    return view('welcome', ['nome' => $nome, 'idade' => $idade, 'profissao' => 'programador']);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/produtos', function () {
+    return view('products');
+});
