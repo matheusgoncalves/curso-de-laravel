c1ad7b39f1c977b7048b0cdbd7fa3152508ed8b2
Matheus <msgoncalves@inf.ufpel.edu.br>
2023-12-02 21:53:27 -0300

    Up to class 17 (uploading images with Laravel)

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -8,20 +8,22 @@ use App\Models\Event;
 
 class EventController extends Controller
 {
-    
+
     public function index()
     {
 
         $events = Event::all();
 
-        return view('welcome',['events' => $events]);
+        return view('welcome', ['events' => $events]);
     }
 
-    public function create() {
+    public function create()
+    {
         return view('events.create');
     }
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
 
         $event = new Event;
 
@@ -30,7 +32,21 @@ class EventController extends Controller
         $event->private = $request->private;
         $event->description = $request->description;
 
-        $event-> save();
+        // Image upload
+        if($request->hasFile('image') && $request->file('image')->isValid()) {
+
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+            $requestImage->move(public_path('img/events'), $imageName);
+
+            $event->image = $imageName;
+        }
+
+        $event->save();
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
     }
