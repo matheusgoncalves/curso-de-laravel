b616269caf45063e10166182728a3ed95d3ae735
Matheus <msgoncalves@inf.ufpel.edu.br>
2023-12-03 17:13:03 -0300

    Up to class 24 (displaying user data in view)

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Models\Event;
 
+use App\Models\User;
+
 class EventController extends Controller
 {
 
@@ -72,6 +74,8 @@ class EventController extends Controller
 
         $event = Event::findOrFail($id);
 
-        return view('events.show', ['event' => $event]);
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
+
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
     }
 }
