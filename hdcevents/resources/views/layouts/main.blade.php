82778012ff3a609d445ea4ec49b8c11282aa886d
Matheus <msgoncalves@inf.ufpel.edu.br>
2023-12-03 16:30:18 -0300

    Up to class 22 (authentication in laravel)

--- hdcevents/resources/views/layouts/main.blade.php
+++ hdcevents/resources/views/layouts/main.blade.php
@@ -32,12 +32,25 @@
                         <li class="nav-item">
                             <a href="/events/create" class="nav-link">Criar Eventos</a>
                         </li>
+                        @auth
                         <li class="nav-item">
-                            <a href="/" class="nav-link">Entrar</a>
+                            <a href="/dashboard" class="nav-link">Meus eventos</a>
                         </li>
                         <li class="nav-item">
-                            <a href="/" class="nav-link">Cadastrar</a>
+                            <form action="/logout" method="post">
+                                @csrf
+                                <a href="/logout" class="nav-link" onclick="event.preventDefault(); this.closest('form').submit();">Sair</a>
+                            </form>
                         </li>
+                        @endauth
+                        @guest
+                            <li class="nav-item">
+                                <a href="/login" class="nav-link">Entrar</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/register" class="nav-link">Cadastrar</a>
+                            </li>
+                        @endguest
                     </ul>
                 </div>
             </nav>
@@ -45,7 +58,7 @@
         <main>
             <div class="container-fluid">
                 <div class="row">
-                    @if(session('msg'))
+                    @if (session('msg'))
                         <p class='msg'>{{ session('msg') }}</p>
                     @endif
                     @yield('content')
